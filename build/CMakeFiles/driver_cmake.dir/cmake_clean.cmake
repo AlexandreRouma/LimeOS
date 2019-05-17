98d5f7924bb8e9ad3df4e963302a9c6c552b215b
Ryzerth <whatsthetgeek@gmail.com>
2019-05-17 18:06:00 +0200

    .gitignore fix 3

--- build/CMakeFiles/driver_cmake.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/driver_cmake"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/driver_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/iso.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/iso"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/iso.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
